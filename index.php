322e54c8e96f7dca689a77eb73ce5e7ef37fca96
naveenchoda <naveenchoda099@gmail.com>
2026-09-09 18:14:51 +0530

    Add initial user management system page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 3 - User Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>User Management System</h1>
+    <p>PHP & MySQL CRUD Application</p>
+
+    <a href="register.php" class="btn">Register</a>
+    <a href="login.php" class="btn">Login</a>
+</div>
+
+</body>
+</html>
